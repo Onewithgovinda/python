590300c21697747673b4eb5f012ab1c8849af80e
Onewithgovinda <onewithgovinda@gmail.com>
2026-06-30 12:41:26 +0530

    python new questions solutions

--- input.py/loop.py
+++ input.py/loop.py
@@ -1,65 +1,121 @@
-# # # # range(stop) -> 0 to stop-1
-# # # for i in range(5):
-# # #     print(i) # 0 1 2 3 4
-# # # # range(start, stop)
-# # # for i in range(2, 6):
-# # #     print(i) # 2 3 4 5
-# # # # range(start, stop, step)
-# # # for i in range(0, 11, 2):
-# # #     print(i) # 0 2 4 6 8 10
-# # # count = 1
-# # # while count<=5:
-# # #     print(count)
-# # #     count+=1
+# # # # # # # range(stop) -> 0 to stop-1
+# # # # # # for i in range(5):
+# # # # # #     print(i) # 0 1 2 3 4
+# # # # # # # range(start, stop)
+# # # # # # for i in range(2, 6):
+# # # # # #     print(i) # 2 3 4 5
+# # # # # # # range(start, stop, step)
+# # # # # # for i in range(0, 11, 2):
+# # # # # #     print(i) # 0 2 4 6 8 10
+# # # # # # count = 1
+# # # # # # while count<=5:
+# # # # # #     print(count)
+# # # # # #     count+=1
 
 
-# # # search for a number
-# # # for n in [2, 4, 6, 8]:
-# # #     if n == 5:
-# # #         print("Found 5!")
-# # #     break
+# # # # # # search for a number
+# # # # # # for n in [2, 4, 6, 8]:
+# # # # # #     if n == 5:
+# # # # # #         print("Found 5!")
+# # # # # #     break
+# # # # # # else:
+# # # # # #     print("5 is not in the list")c
+# # # # # n = int(input("Enter a number to print its multiplication table: "))
+# # # # # for i in range(1,11,1):
+# # # # #     print(f"{n}*{i}={n*i}")
+
+# # # # counter = 5
+# # # # while counter >0:
+# # # #     print(counter)
+# # # #     counter -=1
+# # # #     if counter == 0:
+# # # #         print("let's go!")
+
+
+
+# # # # • Print the sum of numbers from 1 to 100 using a loop.
+# # # # • Print only even numbers from 1 to 20 using continue.
+# # # # • Take a number and check if it is prime, using a loop with break
+
+# # # # sum =0
+# # # # for i in range(1,101,1):
+# # # #     sum += i
+# # # # print(sum)
+
+# # # # for i in range(1,21,1):
+# # # #     if i%2==0:
+# # # #         print(i)
+    
+# # # nums = int(input("Enter a number to check if it is prime or not: "))
+# # # if nums <= 1:
+# # #     print(f"{nums} is not a prime number.")
 # # # else:
-# # #     print("5 is not in the list")c
-# # n = int(input("Enter a number to print its multiplication table: "))
-# # for i in range(1,11,1):
-# #     print(f"{n}*{i}={n*i}")
-
-# counter = 5
-# while counter >0:
-#     print(counter)
-#     counter -=1
-#     if counter == 0:
-#         print("let's go!")
-
-
-
-# • Print the sum of numbers from 1 to 100 using a loop.
-# • Print only even numbers from 1 to 20 using continue.
-# • Take a number and check if it is prime, using a loop with break
-
-# sum =0
-# for i in range(1,101,1):
-#     sum += i
-# print(sum)
-
-# for i in range(1,21,1):
-#     if i%2==0:
-#         print(i)
-    
-nums = int(input("Enter a number to check if it is prime or not: "))
-if nums <= 1:
-    print(f"{nums} is not a prime number.")
-else:
-    is_prime = True
-    for i in range(2,nums-1):
-        if nums%2==0:
-            is_prime = False
-            break
+# # #     is_prime = True
+# # #     for i in range(2,nums-1):
+# # #         if nums%2==0:
+# # #             is_prime = False
+# # #             break
     
 
-if is_prime:
-    print(f"{nums} is a prime number.")
-else:
-    print(f"{nums} is not a prime number.")
+# # # if is_prime:
+# # #     print(f"{nums} is a prime number.")
+# # # else:
+# # #     print(f"{nums} is not a prime number.")
 
+# # # num = int(input("Enter a number to check if it is prime or not: "))
+# # # product = 1
+# # # for i in range(1,num+1,1):
+# # #     product *= i
+# # # print(f"The factorial of {num} is: {product}")
 
+# # # n = int(input("Enter the number: "))
+# # # original_n = n
+# # # sum =0;
+# # # while n > 0:
+# # #     rem = n%10
+# # #     sum = sum + rem**3
+# # #     n = n//10
+
+# # # if sum == original_n:
+# # #     print(f"{original_n} is an Armstrong number.")
+# # # else:
+# # #     print(f"{original_n} is not an Armstrong number.")
+
+# # a =0
+# # b=1
+# # n = int(input("Enter the number of terms in the Fibonacci series: "))
+# # for i in range(n):
+
+# #     c = a+b
+# #     a=b
+# #     b=c
+# #     print(b, end=" ")
+# #i =1
+n= int(input("Enter the number "))
+# # while(n+1>0):
+# #     if n%i==0:
+# #      print(i)
+# #     i+=1
+
+# for i in range(n,0,-1) :
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(i):
+#          print("*",end="")
+#     print()
+
+# rows = 5
+
+# for i in range(rows, 0, -1):
+#     for j in range(rows - i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+for i in range(1, n):
+    c = 'A'
+    for j in range(1, i ):
+        print(c, end="")
+        c = chr(ord(c) + 1)
+    
---
+++ input.py/python list .py/main.py
@@ -0,0 +1,114 @@
+# # # # # # # # # # Empty list
+# # # # # # # # # items = []
+# # # # # # # # # # List of numbers
+# # # # # # # # # marks = [85, 90, 72, 60]
+# # # # # # # # # # Mixed data types allowed
+# # # # # # # # # mixed = [1, "Rajeev", 3.14, True]
+# # # # # # # # # # Using the list() constructor
+# # # # # # # # # letters = list("python") # ['p','y','t','h','o','n']
+# # # # # # # # # print(items)
+# # # # # # # # # print(marks)
+# # # # # # # # # print(mixed)
+# # # # # # # # # print(letters)
+
+
+
+# # # # # # # # fruits = ["apple", "banana", "cherry", "mango"]
+# # # # # # # # # 0 1 2 3
+# # # # # # # # # -4 -3 -2 -1
+# # # # # # # # print(fruits[0]) # apple
+# # # # # # # # print(fruits[2]) # cherry
+# # # # # # # # print(fruits[-1]) # mango (last item)
+# # # # # # # # print(fruits[-2]) # cherry
+
+
+
+# # # # # # # # nums = [10, 20, 30, 40, 50, 60]
+# # # # # # # # print(nums[1:4]) # [20, 30, 40]
+# # # # # # # # print(nums[:3]) # [10, 20, 30] from start
+# # # # # # # # print(nums[3:]) # [40, 50, 60] till end
+# # # # # # # # print(nums[::2]) # [10, 30, 50] every 2nd
+# # # # # # # # print(nums[::-1]) # [60,50,40,30,20,10] reversed!
+
+# # # # # # # # # print(nums[0:7:2])
+
+
+# # # # # # # tasks = ["code", "test"]
+# # # # # # # tasks.append("deploy") # ['code','test','deploy']
+# # # # # # # #tasks.insert(1, "review") # ['code','review','test','deploy']
+# # # # # # # #tasks.remove("test") # ['code','review','deploy']
+# # # # # # # last = tasks.pop() # last = 'deploy'
+# # # # # # # #tasks[0] = "design" # ['design','review']
+# # # # # # # print(tasks)
+# # # # # # # print(tasks.append("deploy"))
+# # # # # # # # print(last)
+# # # # # # # # print(len(tasks)) # 2
+# # # # # # # # print("review" in tasks) # True
+# # # # # # # # print("test" not in tasks) # True
+# # # # # # # # print(tasks.index("review")) # 1
+# # # # # # # # print(tasks.count("review")) # 1
+
+
+# # # # # # matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+# # # # # # print(matrix[0]) # [1, 2, 3]
+# # # # # # print(matrix[1][2]) # 6 (row 1, col 2)
+# # # # # # print(matrix[2][2])
+
+
+# # # # # # point = [10, 20, 30,78,89]
+# # # # # # x, y, *z = point
+# # # # # # print(x, y, z) # 10 20 30
+
+# # # # # # # Star (*) captures the rest
+# # # # # # first, *rest = [1, 2, 3, 4, 5]
+# # # # # # print(first) # 1
+# # # # # # print(rest) # [2, 3, 4, 5]
+
+# # # # # a = [1, 2, 3]
+# # # # # b = a # WRONG — same list!
+# # # # # print(a) # [1, 2, 3]
+# # # # # b.append(4)
+# # # # # print(a) # [1, 2, 3, 4] a changed too
+# # # # # # Correct ways to copy:
+# # # # # b = a.copy() # method 1
+# # # # # b = a[:] # method 2 (full slice)
+# # # # # b = list(a) # method 3
+# # # # # print(b) # [1, 2, 3, 4]
+# # # # # print(a) # [1, 2, 3, 4]
+
+
+# # # # # a =["python", "java", "c++", "rust", "go"]
+
+# # # # # print(a[0])
+# # # # # print(a[-5])
+# # # # # print(a[-1])
+# # # # # print(a[4])
+
+
+
+# # # # marks = [45, 67, 89, 23, 90, 56, 78] 
+# # # # print(marks[0:3]) # [45, 67, 89, 23]
+# # # # print(marks[5:]) # [23, 90, 56, 78]
+# # # # print(marks[::2]) # [45, 89, 90, 78]
+# # # # print(marks[::-1]) # [78, 56, 90, 23, 89, 67, 45]
+
+# # # todos = ["wake up", "study"]
+# # # todos.append("exercise")
+# # # todos.insert(1,"meditate")
+# # # todos.remove("study")
+# # # todos[-1]="sleep"
+# # # print(todos) # ['wake up', 'meditate', 'sleep']
+# # a = [["raj","jay","van"],[34,45,67]]
+# # print(a[0][1]) # jay
+# # print(a[1][2])
+
+# data = [101, "Varanasi", 25.3, 82.9]
+# id, city, lat, lon = data
+# print(f"{id}, {city}, {lat}, {lon}")
+
+
+original = [1, 2, 3]
+backup = original.copy() 
+print(backup.append(99))
+print(backup) # [1, 2, 3, 99]
+print(original) # [1, 2, 3]
