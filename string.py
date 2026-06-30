1abd0c1a0639359c0a3d1ec2540a24c22f45cbc4
Onewithgovinda <onewithgovinda@gmail.com>
2026-06-30 14:55:34 +0530

    save changes and exit

--- string.py
+++ string.py
@@ -2,53 +2,53 @@ s="Hello world"
 
 
 
-# d = "PYTHON"
-# print(d)
-# new_d="J"+d[1::]
-# print(new_d)
-# 
-
-# name = "govinda"
-# print("hello "*4)
-# line = '-'*30
-# print(line)
+d = "PYTHON"
+print(d)
+new_d="J"+d[1::]
+print(new_d)
 
 
-# 
-
-
-# s1=' i love java\n '
-# s2='but i love python too '
-# print("love "not in  s1)
-# for ch in "PY":
-# print(ch, end=" ") # P Y
-# 
-
-# 
-# s='I love Python programming'
-# s1= 'in the funny way'
-# print(s+" "+s1 )
-# line = '*'
-
-# print(line*30)
-# s =  'PyMaster India'
-# b='my name is govinda'
-# c = 'im a boy'
-# print(s[1:-1])
-# print(b[1:-1])
-# print(b[1:-1])
-
-# print('he say \"Hello\"')
-# #C:\Users\Rajeev\Desktop using escape characters.
-# print("C:\\Users\\Rajeev\\Desktop using escape Characters.")
+name = "govinda"
+print("hello "*4)
+line = '-'*30
+print(line)
 
 
 
 
-# s =  'PYTHON'
-# b=""
-# # print(s[:])
-# # print(s[1:1])
-# print(s[5:2])
+
+s1=' i love java\n '
+s2='but i love python too '
+print("love " not in s1)
+for ch in "PY":
+    print(ch, end=" ") # P Y
+
+
+
+s='I love Python programming'
+s1= 'in the funny way'
+print(s+" "+s1 )
+line = '*'
+
+print(line*30)
+s =  'PyMaster India'
+b='my name is govinda'
+c = 'im a boy'
+print(s[1:-1])
+print(b[1:-1])
+print(b[1:-1])
+
+print('he say \"Hello\"')
+#C:\Users\Rajeev\Desktop using escape characters.
+print("C:\\Users\\Rajeev\\Desktop using escape Characters.")
+
+
+
+
+s =  'PYTHON'
+b=""
+# print(s[:])
+# print(s[1:1])
+print(s[5:2])
 s = 'X' + s[1:]
 print(s)
