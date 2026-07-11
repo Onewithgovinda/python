48e792ef194f128298ee65fdb80a7df86e4ffddb
Onewithgovinda <onewithgovinda@gmail.com>
2026-07-11 21:11:20 +0530

    input.py/waest.py

---
+++ input.py/waest.py
@@ -0,0 +1,12 @@
+num=[1,2,3,4,5]
+print(num)
+from collections import defaultdict # count letters - the messy normal-dict way 
+counts = {}
+for ch in "banana": 
+    if ch in counts: counts [ch] += 1 
+    else: counts[ch] = 1 
+# the clean defaultdict way counts = defaultdict (int) for ch in "banana": counts[ch] += 1 print(dict(counts))
+
+# missing keys start at 0
+
+# {'b' : 1, 'a': 3, 'n':
